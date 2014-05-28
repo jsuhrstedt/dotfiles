ace5f518e61ab5642e83e185139321e70124ac32
jeremy <jeremy@element.(none)>
2014-05-27 20:35:04 -0700

    changed zsh prompt

---
+++ .zsh/lib/prompt.zsh
@@ -0,0 +1,50 @@
+# autoload -U add-zsh-hook
+autoload -Uz vcs_info
+
+# # enable VCS systems you use
+zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:*:prompt:*' check-for-changes true
+
+setopt prompt_subst
+autoload -U promptinit
+promptinit
+
+
+# Look at http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
+# for mor options
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '*'   # display this when there are unstaged changes
+zstyle ':vcs_info:*' stagedstr '+'  # display this when there are staged changes
+zstyle ':vcs_info:*' actionformats '[%b%|%a%c%u%]%f'
+zstyle ':vcs_info:*' formats ':%b%c%u%f'
+
+
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+precmd () { vcs_info }
+
+PROMPT='[%n@%m %~${vcs_info_msg_0_}] %{$reset_color%}'
+
+vim_ins_mode="%{$fg[cyan]%}[INS]%{$reset_color%}"
+vim_cmd_mode="%{$fg[green]%}[CMD]%{$reset_color%}"
+vim_mode=$vim_ins_mode
+
+function zle-keymap-select {
+  vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
+  zle reset-prompt
+}
+zle -N zle-keymap-select
+
+function zle-line-finish {
+  vim_mode=$vim_ins_mode
+}
+zle -N zle-line-finish
+
+# Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
+# Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
+# Thanks Ron! (see comments)
+function TRAPINT() {
+  vim_mode=$vim_ins_mode
+  return $(( 128 + $1 ))
+}
+
+RPROMPT='${vim_mode}'
